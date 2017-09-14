b0a605cc9b3e9a3a9c87386d2553ea7e910dc327
impersin <closer124@gmail.com>
2017-09-14 14:28:44 -0700

    Added condition to show item list only with input text

--- App/components/ConnectSearchBox.js
+++ App/components/ConnectSearchBox.js
@@ -5,17 +5,20 @@ import {
   Modal, 
   Text, 
   TextInput, 
-  TouchableHighlight, 
+  TouchableHighlight,
+  TouchableOpacity, 
   View
  } from 'react-native';
 
 import { connectSearchBox } from 'react-instantsearch/connectors';
 
-const SearchBox = connectSearchBox(({ refine, currentRefinement }) => {
+const SearchBox = connectSearchBox(({ refine, currentRefinement, props }) => {
     
   const styles = {
-    height: 60,
-    borderWidth: 1,
+    height: 50,
+    borderWidth: 2,
+    borderRadius: 4,
+    borderColor: '#ffffff',
     padding: 10,
     margin: 10,
     flex: 1,
@@ -24,7 +27,15 @@ const SearchBox = connectSearchBox(({ refine, currentRefinement }) => {
   return (
         <TextInput
           style={styles}
-          onChangeText={text => refine(text)}
+          onChangeText={(text) => { 
+            if (text.length === 1) {
+              props.showItemList();
+            }
+            if (text.length === 0) {
+              props.HideItemList();
+            }
+            refine(text); 
+          }}
           value={currentRefinement}
           placeholder={'Search a product...'}
           clearButtonMode={'always'}
