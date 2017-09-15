3588964198e31af4795c70ab1acb65c5a8fe9d89
Scott Chao <svinets@gmail.com>
2017-09-15 14:48:33 -0700

    Create profile page for items

---
+++ App/components/ItemProfile.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Image, Text, TouchableHighlight, View, StyleSheet } from 'react-native';
+
+const binType = (color) => {
+  //refine as needed
+  let type, icon;
+  switch (color) {
+  case 'blue': type = 'Recycle', icon = 'http://www.recycling.com/wp-content/uploads/2016/06/recycling-symbol-icon-twotone-dark-blue.png';
+    break;
+  case 'green': type = 'Compost', icon = 'http://www.recycling.com/wp-content/uploads/2016/06/recycling-symbol-icon-twotone-dark-green.png';
+    break;
+  case 'black': type = 'Waste', icon = 'http://www.recycling.com/wp-content/uploads/2016/06/recycling-symbol-icon-twotone-black.png';
+    break;    
+  }
+  return [type, icon];
+};
+
+const styles = StyleSheet.create({
+  tableCell: {
+    padding: 5,
+    borderWidth: 1,
+    borderStyle: 'dotted',
+    borderColor: 'black',
+  }
+});
+
+export default Profile = ({ item }) => (
+  <View style={{ flexDirection: 'column', marginTop: 30 }}>
+    <Text style={{ fontSize: 20, margin: 10 }}>{item.name}</Text>
+    <Image style={{ height: 200, width: 300 }} source={{ uri: item.imgUrl }} />
+    <View style={{ flexDirection: 'row' }}>
+      <View style={[styles.tableCell, { height: 100, width: '50%', alignItems: 'center' }]}>
+        <Image style={{ height: 70, width: 70 }} source={{ uri: binType(item.bin)[1] }} />
+        <Text>{binType(item.bin)[0]}</Text>
+      </View>
+      <View style={[styles.tableCell, { height: 100, width: '50%', alignItems: 'center' }]}>
+        <Text>placeholder</Text>
+      </View>
+    </View>
+    <View style={ styles.tableCell }>
+      <Text>Brand name: {item.brand}</Text>
+    </View>      
+    <View style={ styles.tableCell }>
+      <Text>Material type: {item.material}</Text>
+    </View>
+  </View>
+);
