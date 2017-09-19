86e81e37bb82f4b973810cb294a1f20cda6de9bf
impersin <closer124@gmail.com>
2017-09-19 16:02:30 -0700

    Added FormView page to StackNavigator

--- App/components/App.js
+++ App/components/App.js
@@ -1,36 +1,20 @@
 import React, {Component} from 'react';
 import Landing from './../containers/LandingContainer';
 import Profile from './../containers/ProfileContainer';
+import FormView from './../components/FormView';
 import { StyleSheet, FlatList, Image, Modal, Text, TextInput, TouchableHighlight, View, } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 const App = StackNavigator({
-  Landing: { screen: Landing },
+  Landing: { 
+    screen: Landing },
   Profile: { screen: Profile },
+  FormView: { screen: FormView },
 });
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Landing />
-//       </View>
-//     );
-//   }
-// }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#48d1cc'
-  },
-  direction: {
-    flexDirection: 'row', 
-    alignItems: 'center'
   }
 });
 
